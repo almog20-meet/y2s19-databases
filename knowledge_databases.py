27abf1ef3fa2a7a484e82212cf337042a7d0879f
myname21-meet <myname21@meet.mit.edu>
2019-07-29 07:15:00 -0400

    labs

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -1,27 +1,55 @@
-from knowledge_model import Base, Knowledge
+from knowledge_model import Base, People
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///knowledge.db')
+engine = create_engine('sqlite:///People.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(name, birth_year, alive, rate ):
+        object_f=People(
+                name = name,
+                birth_year = birth_year,
+                alive = alive,
+                rate = rate)
+        session.add(object_f)
+        session.commit()
+        
+add_article("Michael Jackson", 1958, False, 100)
+add_article("Barack Obama", 1961, True, 90)
+add_article("Britney Spears",1981, True, 95)
 
 def query_all_articles():
-	pass
+        articles = session.query(People).all()
+        return articles
+print (query_all_articles())
 
-def query_article_by_topic():
-	pass
+def query_article_by_topic(thier_name):
+        articles = session.query(People).filter_by(name=thier_name)
+        return articles
+print(query_article_by_topic("Michael Jackson"))
 
-def delete_article_by_topic():
-	pass
+def delete_article_by_topic(name):
+	session.query(People).filter_by(
+                name=name).delete()
+	session.commit()
+delete_article_by_topic("Barack Obama")
 
 def delete_all_articles():
-	pass
+        session.query(People).delete()
+        session.commit()
+delete_all_articles()
 
-def edit_article_rating():
-	pass
+"""
+def edit_article_rating(name, alive):
+        object_f = session.query(
+                People).filter_by(
+                name=name).first()
+        object_f.alive = alive
+        session.commit()
+edit_article_rating("Britney Spears", False)
+"""
+
+print (query_all_articles())
--- knowledge_model.py
+++ knowledge_model.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Knowledge(Base):
+class People(Base):
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
@@ -14,4 +14,20 @@ class Knowledge(Base):
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
 
-	pass
+        __tablename__ = "famous peoples"
+        table_id = Column(Integer, primary_key=True)
+        name = Column (String)
+        birth_year = Column (Integer)
+        alive = Column (Boolean)
+        rate = Column (Integer)
+        def __repr__(self):
+                return (" Famous Name: {}\n"
+                        " Famous birth year : {}\n"
+                        "Famous is live : {}\n"
+                        "rating: {}\n" ). format (
+                                self.name, self.birth_year, self.alive, self.rate)
+
+x = People( name = "Michael Jackson" , birth_year= 1958, alive= False, rate = 10000)
+print (x)
+
+
